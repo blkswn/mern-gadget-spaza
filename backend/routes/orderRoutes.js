43b64b15f92e7572e644b9090c9493d5fcb28fae
blkswn <kiddwretch@gmail.com>
2023-06-29 17:37:37 +0200

    changed put not post

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -7,7 +7,7 @@ import {protect, admin} from '../middleware/authMiddleware.js'
 router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders)
 router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
-router.route('/:id/payfast').put(updateOrderToPaid)
+router.route('/:id/payfast').post(updateOrderToPaid)
 router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered)
 
 // Add the new route for PayFast notification callback
